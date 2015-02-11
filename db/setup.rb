535843a5ae5ff8f82572abd9cf7fe5021ecdfe0f
James Dabbs <jamesdabbs@gmail.com>
2015-02-11 07:11:48 -0500

    Add flexible database config

--- db/setup.rb
+++ db/setup.rb
@@ -2,4 +2,12 @@ require 'active_record'
 require 'yaml'
 
 db_config = YAML::load(File.open('config/database.yml'))
+
+env_config = if ENV["TEST"]
+  db_config[:test]
+else
+  db_config[:development]
+end
+
+raise "Could not find database config for environment" unless db_config
 ActiveRecord::Base.establish_connection(db_config)
