1699f708e135ebd1fdfabf5386f9739a9e78a277
James Dabbs <jamesdabbs@gmail.com>
2015-02-11 13:59:04 -0500

    Unbreak db setup

--- db/setup.rb
+++ db/setup.rb
@@ -4,10 +4,10 @@ require 'yaml'
 db_config = YAML::load(File.open('config/database.yml'))
 
 env_config = if ENV["TEST"]
-  db_config[:test]
+  db_config["test"]
 else
-  db_config[:development]
+  db_config["development"]
 end
 
-raise "Could not find database config for environment" unless db_config
-ActiveRecord::Base.establish_connection(db_config)
+raise "Could not find database config for environment" unless env_config
+ActiveRecord::Base.establish_connection(env_config)
