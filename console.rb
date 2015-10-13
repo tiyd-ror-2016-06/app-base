bf7015ddb227c47043fe69e2ce1c64c03f88c3bf
James Dabbs <jamesdabbs@gmail.com>
2015-10-13 13:17:22 -0400

    Include did_you_mean

--- console.rb
+++ console.rb
@@ -1,4 +1,3 @@
-require "pry"
 require "./db/setup"
 require "./lib/all"
 
--- lib/all.rb
+++ lib/all.rb
@@ -1,1 +1,4 @@
+require "pry"
+require "did_you_mean"
+
 Dir["#{File.dirname __FILE__}/**/*.rb"].each { |path| require path }
