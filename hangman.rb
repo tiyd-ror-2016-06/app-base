baa8db6dcfa7ce0e10050c96814e5865e20bddb2
Zach H <zdhadgraft@gtcc.edu>
2016-06-06 23:27:43 -0400

    Modified the way guesses are handled; moved guess branching inside Game class.

--- hangman.rb
+++ hangman.rb
@@ -37,27 +37,62 @@ class Game
     @hints_allowed      = hints_allowed
     @hints_remaining    = hints_remaining
     @correct_guesses    = "_" * word.length
+    @all_guesses        = []
 
     def over?
       @tries_remaining == 0 || word_is_found?
     end
 
     def word_is_found?
-      @word == @correct_guesses
+      x = @word.split("").select { |letter| not @all_guesses.include? letter }
+      x.empty?
     end
 
-    def print_board(revealed:)
-      @word.split("").reduce 0 do | accum, letter|
-        if revealed
+    def print_board(reveal_answers:)
+      @word.split("").each do | letter |
+        if reveal_answers || (@all_guesses.include? letter)
           print letter
         else
-          print @correct_guesses[accum]
+          print "_"
         end
         print " "
-        accum += 1
       end
       print "\n"
     end
+
+    def print_status
+      puts
+      puts "#{@tries_remaining} guesses left"
+      puts "#{@hints_remaining} left"
+      puts
+    end
+
+    def print_options
+      print "What is your guess? (Enter '?' for a hint; CTRL + C to quit)"
+    end
+
+    def prompt_user
+      guess = gets.chomp.to_s.downcase
+      if guess == @word
+        amazing_guess
+      elsif guess == "?"
+        get_a_hint ||
+          "You have no more hints!"
+        sleep 1
+      elsif @all_guesses.include? guess
+        puts "You already guessed that."
+        sleep 1
+      elsif not ("a".."z").include? guess
+        puts "That is not a valid character."
+        sleep 1
+      else
+        @all_guesses.push guess
+        @tries_remaining -= 1
+      end
+    end
+
+
+
 
   end
 end
@@ -117,13 +152,6 @@ def print_title #(guesses_allowed, hints, correct_guesses)
   puts "H A N G M A N"
   puts
 
-#  print_score correct_guesses
-
-#  puts ""
-#  puts ""
-#  puts "#{guesses_allowed} guesses left"
-#  puts "#{hints} hints left"
-#  puts
 #  print "What is your guess? (Enter '?' for a hint) "
 
 end
@@ -199,14 +227,13 @@ def new_game word_bank, game_options
 
   until g.over?
     print_title
-    g.print_board   revealed: false
-    binding.pry
+    g.print_board   reveal_answers: false
     g.print_status
     g.print_options
     g.prompt_user
   end
   print_title
-  g.print_board     revealed: true
+  g.print_board     reveal_answers: true
   g.print_outcome
   g.save
   #play_again?
